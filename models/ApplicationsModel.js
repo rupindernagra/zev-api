8de2708ee3bcc02c6e44fae46991eefc2cd5218f
bhavandeepsingh <bhavandeepboparai@yahoo.co.in>
2019-11-26 19:19:00 +0530

    changes in api

--- models/ApplicationsModel.js
+++ models/ApplicationsModel.js
@@ -8,10 +8,10 @@ class ApplicationsModel extends BaseModel {
             id: { 'type': 'number', 'required': false, 'value': 0 },
             firstname: { 'type': 'text', 'required': true, 'value': '', 'error': 'First Name is Required' },
             lastname: { 'type': 'text', 'required': false, 'value': '' },
-            space_id: { 'type': 'number', 'required': true, 'value': '0', 'error': 'Space Id is required' },
+            space_id: { 'type': 'number', 'required': true, 'value': 0, 'error': 'Space Id is required' },
             email: { 'type': 'email', 'required': true, 'value': '', 'error': 'Email is Required!' },
             phone: { 'type': 'phone', 'required': false, 'value': '' },
-            message: { 'type': 'text', 'required': true, 'value': '' },
+            message: { 'type': 'text', 'required': false, 'value': '' },
             reports: { 'type': 'text', 'required': false, 'value': '' }
         }
         this._table = "applications"
